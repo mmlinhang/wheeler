91fb593fd7f4833f846832bf05e531e33183eb87
mmlinhang <1105435102@qq.com>
2018-09-25 22:39:53 +0800

    项目初始化

---
+++ mvc-wheel/src/test/java/Log4jTest.java
@@ -0,0 +1,13 @@
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class Log4jTest {
+
+    @Test
+    public void testLog4j() {
+        Logger logger = Logger.getLogger(Log4jTest.class);
+        logger.debug("it is debug logger");
+        logger.info("it is info logger");
+        logger.error("it is error logger");
+    }
+}
